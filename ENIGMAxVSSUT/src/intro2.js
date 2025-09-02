a85525b56ae4f80413261d3e4d6c4920656b8d14
raopremrao <raom61337@gmail.com>
2025-09-02 17:35:25 +0530

    Added cursor in Hero page

--- ENIGMAxVSSUT/src/intro2.js
+++ ENIGMAxVSSUT/src/intro2.js
@@ -5,6 +5,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 const heroAnimation = gsap.to('.hero img', {
     scale: 1.5,
+    // opacity: 0,
     ease: 'none',
 })
 
@@ -17,3 +18,30 @@ ScrollTrigger.create({
   animation: heroAnimation
 })
 
+
+const cursor = document.querySelector('.cursor');
+const hero = document.querySelector('.hero')
+
+hero.addEventListener('mousemove', (dets) => {
+    gsap.to(cursor, {
+        opacity: 1,
+        display: "flex",
+        x: dets.x + 10,
+        y: dets.y + 10,
+        duration: 1,
+        // ease: "back.out(4)"
+        ease: 'Power4.out'
+    })
+})
+
+hero.addEventListener('mouseleave', (dets) => {
+    gsap.to(cursor, {
+        opacity: 0,
+        display: "none",
+        // x: dets.x + 10,
+        // y: dets.y + 10,
+        duration: 1,
+        // ease: "back.out(4)"
+        ease: 'Power4.out'
+    })
+})
